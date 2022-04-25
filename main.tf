f41a25c1a04b94e5fa53f75396feea2190ebfde0
prafulpatel16 <praful1611@outlook.com>
2022-04-24 20:49:17 -0600

    userdata updated with new

--- main.tf
+++ main.tf
@@ -147,7 +147,7 @@ resource "oci_core_instance" "webserver01" {
 
   metadata = {
     ssh_authorized_keys = var.ssh_public_key1
-    user_data           = templatefile("user_data_web01.tfpl")
+    user_data = base64encode(var.user-data)
     
   }
 }
@@ -173,8 +173,32 @@ resource "oci_core_instance" "webserver02" {
 
   metadata = {
     ssh_authorized_keys = var.ssh_public_key2
-    user_data           = templatefile("user_data_web02.tfpl")
-
+    user_data = base64encode(var.user-data)
    
   }
+}
+
+variable "user-data" {
+  default = <<EOF
+#!/bin/bash -x
+echo '################### webserver userdata begins #####################'
+touch ~opc/userdata.`date +%s`.start
+# echo '########## yum update all ###############'
+# yum update -y
+echo '########## basic webserver ##############'
+yum install -y httpd
+systemctl enable  httpd.service
+systemctl start  httpd.service
+echo '<html><head></head><body><pre><code>' > /var/www/html/index.html
+hostname >> /var/www/html/index.html
+echo '' >> /var/www/html/index.html
+cat /etc/os-release >> /var/www/html/index.html
+echo '</code></pre></body></html>' >> /var/www/html/index.html
+firewall-offline-cmd --add-service=http
+systemctl enable  firewalld
+systemctl restart  firewalld
+touch ~opc/userdata.`date +%s`.finish
+echo '################### webserver userdata ends #######################'
+EOF
+
 }
