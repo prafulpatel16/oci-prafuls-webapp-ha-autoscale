9d9f07618ca1a8967e160633776ca476970b935b
prafulpatel16 <praful1611@outlook.com>
2022-04-26 22:20:14 -0600

    updated code with tow subnets

--- main.tf
+++ main.tf
@@ -1,6 +1,17 @@
 // Copyright (c) 2017, 2021, Oracle and/or its affiliates. All rights reserved.
 // Licensed under the Mozilla Public License v2.0
+#-------------------------------------------------------------------------------------------------
+# Purpose: Deploy Highly Available web application using Load Balancer and Auto scaling inside OCI
+# Application Name: Praful's Professional Portfolio 
+# Infrastructure Automation: Terraform
+# Version Control: GitHub
+# State Managment & Pipeline: Terraform Cloud
+# ------------------------------------------  
+# Author: Praful Patel
+# Date & Time: Apr 24, 2022 
+# ------------------------------------------
 
+#Define Variables
 
 variable "compartment_ocid" {}
 variable "region" {}
@@ -33,6 +44,7 @@ variable "ad_region_mapping" {
   }
 }
 
+# Image mapping
 variable "images" {
   type = map(string)
 
@@ -45,11 +57,32 @@ variable "images" {
   }
 }
 
+# Define AD
 data "oci_identity_availability_domain" "ad" {
   compartment_id = var.tenancy_ocid
   ad_number      = var.ad_region_mapping[var.region]
 }
 
+data "oci_identity_availability_domain" "ad1" {
+  compartment_id = var.tenancy_ocid // needs to be compartment_ocid if not using root compartment
+  ad_number      = 1
+}
+
+data "oci_identity_availability_domain" "ad2" {
+  compartment_id = var.tenancy_ocid // needs to be compartment_ocid if not using root compartment
+  ad_number      = 2
+}
+
+################################################################################################################
+# Networking Components
+#1. Create Virtual Cloud Network
+#2. Create Subnet
+#3. Create Internet Gateway
+#4. Create Route Table
+#5. Create Security List
+
+
+#1. Create Virtual Cloud Network
 resource "oci_core_virtual_network" "prp_vcn" {
   cidr_block     = "10.1.0.0/16"
   compartment_id = var.compartment_ocid
@@ -57,6 +90,7 @@ resource "oci_core_virtual_network" "prp_vcn" {
   dns_label      = "prpvcn"
 }
 
+#2. Create Subnet
 resource "oci_core_subnet" "prp_subnet_one" {
   cidr_block        = "10.1.20.0/24"
   display_name      = "prpsubnet1"
@@ -68,12 +102,24 @@ resource "oci_core_subnet" "prp_subnet_one" {
   dhcp_options_id   = oci_core_virtual_network.prp_vcn.default_dhcp_options_id
 }
 
+resource "oci_core_subnet" "prp_subnet_two" {
+  cidr_block        = "10.1.30.0/24"
+  display_name      = "prpsubnet2"
+  dns_label         = "prpsubnet2"
+  security_list_ids = [oci_core_security_list.prp_security_list.id]
+  compartment_id    = var.compartment_ocid
+  vcn_id            = oci_core_virtual_network.prp_vcn.id
+  route_table_id    = oci_core_route_table.prp_route_table.id
+  dhcp_options_id   = oci_core_virtual_network.prp_vcn.default_dhcp_options_id
+}
+#3. Create Internet Gateway
 resource "oci_core_internet_gateway" "prp_internet_gateway" {
   compartment_id = var.compartment_ocid
   display_name   = "prpIGW"
   vcn_id         = oci_core_virtual_network.prp_vcn.id
 }
 
+#4. Create Route Table
 resource "oci_core_route_table" "prp_route_table" {
   compartment_id = var.compartment_ocid
   vcn_id         = oci_core_virtual_network.prp_vcn.id
@@ -86,6 +132,7 @@ resource "oci_core_route_table" "prp_route_table" {
   }
 }
 
+#5. Create Security List
 resource "oci_core_security_list" "prp_security_list" {
   compartment_id = var.compartment_ocid
   vcn_id         = oci_core_virtual_network.prp_vcn.id
@@ -116,6 +163,7 @@ resource "oci_core_security_list" "prp_security_list" {
     }
   }
 }
+###############################################################################################
 # Load Balancer Components:
 #1.Add details
 #2.Create Backendset
@@ -129,7 +177,7 @@ resource "oci_load_balancer" "prp-lb" {
   compartment_id = var.compartment_ocid
 
   subnet_ids = [
-    oci_core_subnet.prp_subnet_one.id,
+    oci_core_subnet.prp_subnet_one.id, oci_core_subnet.prp_subnet_two.id
     
   ]
 
@@ -178,10 +226,17 @@ resource "oci_load_balancer_backend" "prp-lb-backend1" {
   offline          = false
   weight           = 1
 }
+#################################################################################################################
 
 # Instance configruation & Instance Pool process
+#1.instance template
+#2.Create custom image from instance-template
+#3.Create Instance Configuration
+#4.Create instance pool
+
 #source: https://github.com/oracle/terraform-provider-oci/blob/master/examples/compute/instance_pool/instance_pool.tf
-#prp-template-instance
+
+#1 Instance template
 resource "oci_core_instance" "prp-template-instance" {
   availability_domain = data.oci_identity_availability_domain.ad.name
   compartment_id      = var.compartment_ocid
@@ -208,6 +263,7 @@ resource "oci_core_instance" "prp-template-instance" {
   }
 }
 
+#2.Create custom image from instance-template
 resource "oci_core_image" "prp_custom_image" {
   compartment_id = var.compartment_ocid
   instance_id    = oci_core_instance.prp-template-instance.id
@@ -219,7 +275,7 @@ resource "oci_core_image" "prp_custom_image" {
 }
 
 
-# Create instance congifurations
+#3.Create instance congifurations
 resource "oci_core_instance_configuration" "prpInstanceConfiguration" {
   compartment_id = var.compartment_ocid
   display_name   = "prpInstanceConfiguration"
@@ -237,7 +293,7 @@ resource "oci_core_instance_configuration" "prpInstanceConfiguration" {
         assign_public_ip       = true
         display_name           = "prpInstanceConfigurationVNIC"
         skip_source_dest_check = false
-        subnet_id              = oci_core_subnet.prp_subnet_one.id
+        subnet_id              = [oci_core_subnet.prp_subnet_one.id, oci_core_subnet.prp_subnet_one.id]
       }
 
       extended_metadata = {
@@ -255,20 +311,21 @@ resource "oci_core_instance_configuration" "prpInstanceConfiguration" {
   }
 }
 
-# Create instance pool
+#4.Create instance pool
 
 resource "oci_core_instance_pool" "prpInstancePool" {
   compartment_id            = var.compartment_ocid
   instance_configuration_id = oci_core_instance_configuration.prpInstanceConfiguration.id
   size                      = 2
   state                     = "RUNNING"
-  display_name              = "prpInstancePool"
+  display_name              = "WebServer"
 
   placement_configurations {
-    availability_domain = data.oci_identity_availability_domain.ad.name
+    availability_domain = [data.oci_identity_availability_domain.ad1.name, ata.oci_identity_availability_domain.ad1.name]
     fault_domains = [
-      "FAULT-DOMAIN-1"]
-    primary_subnet_id   = oci_core_subnet.prp_subnet_one.id
+      FAULT-DOMAIN-1, FAULT-DOMAIN-2]
+    primary_subnet_id   = [oci_core_subnet.prp_subnet_one.id, oci_core_subnet.prp_subnet_two.id]
+   
   }
 
   load_balancers {
@@ -284,16 +341,10 @@ resource "oci_core_instance_pool" "prpInstancePool" {
   
 }
 
-# Test the instance pool instance
-resource "oci_core_instance_pool_instance" "test_instance_pool_instance" {
-  instance_pool_id = oci_core_instance_pool.prpInstancePool.id
-  instance_id = oci_core_instance.prp-template-instance.id
-  decrement_size_on_delete = true
-  auto_terminate_instance_on_delete = false
-}
 
 
-
+##################################################################################################################
+#Create datasets
 data "oci_core_instance_configuration" "prpInstanceConfiguration" {
   instance_configuration_id = oci_core_instance_configuration.prpInstanceConfiguration.id
 }
@@ -307,9 +358,9 @@ data "oci_core_instance_pool_load_balancer_attachment" "prp_instance_pool_load_b
   instance_pool_load_balancer_attachment_id = oci_core_instance_pool.prpInstancePool.load_balancers[0].id
 }
 
-
+###################################################################################################################
 #Auto scaling process
-
+#1. Create Auto Scaling configurations
 resource "oci_autoscaling_auto_scaling_configuration" "prpAutoScalingConfiguration" {
   compartment_id       = var.compartment_ocid
   cool_down_in_seconds = "300"
@@ -369,7 +420,7 @@ resource "oci_autoscaling_auto_scaling_configuration" "prpAutoScalingConfigurati
   }
 }
 
-
+####################################################################################################################
 # User data Variable for deploying webapplication01
 variable "user-data-web01" {
   default = <<EOF
@@ -399,8 +450,8 @@ sudo yum install git -y
 
 # echo '########### Copy web application source code from GIT to apachwe root directory ##########'
 
-sudo git clone https://github.com/prafulpatel16/prafuls-portfolio-webapp1.git
-sudo cp -r prafuls-portfolio-webapp1/src/* /var/www/html/  
+sudo git clone https://github.com/prafulpatel16/cn_project_iPortfolio_praful.git
+sudo cp -r cn_project_iPortfolio_praful/src/* /var/www/html/  
 
 touch ~opc/userdata-web01.`date +%s`.finish
 echo '################### webserver userdata ends ##########################'
@@ -408,7 +459,7 @@ EOF
 
 }
 
-
+#################################################################################################################
 # reserve public ip
 resource "oci_core_public_ip" "prp_reserved_ip" {
   compartment_id = var.compartment_ocid
@@ -418,8 +469,8 @@ resource "oci_core_public_ip" "prp_reserved_ip" {
     ignore_changes = [private_ip_id]
   }
 }
-
-
+#################################################################################################################
+#Output variables
 output "lb_public_ip" {
   value = [oci_load_balancer.prp-lb.ip_address_details]
 }
